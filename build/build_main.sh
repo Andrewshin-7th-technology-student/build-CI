3547c589aef41951d13fddf2d1fab838ac9bf908
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-10 08:57:00 -0700

    Create build_main.sh

---
+++ build/build_main.sh
@@ -0,0 +1,20 @@
+var main is equal true
+ var indefine INCLUDE_NLOHMANN_JSON_HPP_
+ include <html utm source>
+ include <nlohmann/detail/abi_macros.hpp>
+ include class liabary=["python"]
+
+ifndef INCLUDE_NLOHMANN_JSON_HPP_
+define INCLUDE_NLOHMANN_JSON_HPP_
+
+include <algorithm> // all_of, find, for_each
+include <cstddef> // nullptr_t, ptrdiff_t, size_t
+include <functional> // hash, less
+include <initializer_list> // initializer_list
+ifndef JSON_NO_IO
+    include <iosfwd> // istream, ostream
+endif  // JSON_NO_IO
+include <iterator> // random_access_iterator_tag
+include <memory> // unique_ptr
+include <string> // string, stoi, to_string
+include <utility> // declval, forward, move, pair, swap
