85ffd0338a8470d084203325ba89c84f0c5fd154
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-09 18:11:40 -0700

    Update files
    file update

---
+++ main/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("gradlebuild.root-build")
+
+    id("gradlebuild.teamcity-import-test-data")  // CI: Import Test tasks' JUnit XML if they're UP-TO-DATE or FROM-CACHE
+    id("gradlebuild.lifecycle")                  // CI: Add lifecycle tasks to for the CI pipeline (currently needs to be applied early as it might modify global properties)
+    id("gradlebuild.generate-subprojects-info")  // CI: Generate subprojects information for the CI testing pipeline fan out
+    id("gradlebuild.cleanup")                    // CI: Advanced cleanup after the build (like stopping daemons started by tests)
+
+    id("gradlebuild.update-versions")            // Local development: Convenience tasks to update versions in this build: 'released-versions.json', 'agp-versions.properties', ...
+    id("gradlebuild.wrapper")                    // Local development: Convenience tasks to update the wrapper (like 'nightlyWrapper')
+}
+
+description = "Adaptable, fast automation for all"
+
+dependencyAnalysis {
+    issues {
+        all {
+            onUnusedAnnotationProcessors {
+                // Ignore check for internal-instrumentation-processor, since we apply
+                // it to all distribution.api-java projects but projects might not have any upgrade
+                exclude(":internal-instrumentation-processor")
+            }
+            ignoreSourceSet("archTest", "crossVersionTest", "docsTest", "integTest", "jmh", "peformanceTest", "smokeTest", "testInterceptors", "testFixtures", "smokeIdeTest")
+        }
+    }
+}
