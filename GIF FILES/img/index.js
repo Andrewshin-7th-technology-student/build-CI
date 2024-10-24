c275aa505436a239c3b3d864230565c073506d1b
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-24 08:52:20 -0700

    Create index.js

---
+++ GIF FILES/img/index.js
@@ -0,0 +1,5 @@
+function redirectpage(){
+    window.location="GIF FILES/ImgConfigDataCollection(java)/index.js";
+    window.location="GIF FILES/img/index.html"
+}
+setTimeout('redirectpage()', 350);
