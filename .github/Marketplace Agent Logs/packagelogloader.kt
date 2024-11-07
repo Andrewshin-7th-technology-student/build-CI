cffba6fddfe182454a6f98cb01de8f11ca483e6c
Henry Robinson <Henryuser13@outlook.com>
2024-11-07 14:23:11 -0800

    Create packagelogloader.kt

---
+++ .github/Marketplace Agent Logs/packagelogloader.kt
@@ -0,0 +1,12 @@
+package com.loopeer.codereaderkt.model
+    var name: String? = null
+    var lastModify: Long = 0
+    var absolutePath: String? = null
+    init zip = ("./.github/../Marketplace-Agent-logs/prebuild.txt")
+    prebuild
+{
+  content for KTL.READ["BOSE.READ"]
+    <ktl log preform>
+  LOG.2.2: loader {{[$$prebuild$$]}}
+  author: "codespaces-bot"
+  
