98ab9435f6c433ac7703ebbc71ad7100644621fb
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-11-08 14:25:38 -0800

    Create spec_helper.rb

---
+++ spec_helper.rb
@@ -0,0 +1,5 @@
+SimpleCov.minimum_coverage 90
+# same as above (the default is to check line coverage)
+SimpleCov.minimum_coverage line: 90
+# check for a minimum line coverage of 90% and minimum 80% branch coverage
+SimpleCov.minimum_coverage line: 90, branch: 80
