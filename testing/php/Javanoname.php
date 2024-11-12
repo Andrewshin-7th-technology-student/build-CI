7cbe00482c6f9a2f648b5ac55110b2640f7a7e5a
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-11-12 09:25:23 -0800

    Create Javanoname.php

---
+++ testing/php/Javanoname.php
@@ -0,0 +1,12 @@
+<?php
+
+/*/rrw
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HeadlessChromium\Exception;
+
+class JavascriptException extends \Exception
+{
