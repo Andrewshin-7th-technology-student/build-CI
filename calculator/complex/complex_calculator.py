c0421ee1c12ff55d6aa771c94f5cf5332084215b
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-11-14 10:24:23 -0800

    Create complex_calculator.py

---
+++ calculator/complex/complex_calculator.py
@@ -0,0 +1,7 @@
+class ComplexCalculator:
+    def multiply(x, y):
+        return x * y
+    def divide(x, y):
+        if y == 0:
+            return 'Cannot divide by 0'
+        return float(x)/y
